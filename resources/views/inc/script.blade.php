b64572e8abc9adc8bb549e7f3f61420b87d26f22
MissSuccess888 <pochtyascik54321@mail.ru>
2023-11-19 13:51:32 +0300

    В js поправила название

--- resources/views/inc/script.blade.php
+++ resources/views/inc/script.blade.php
@@ -4,7 +4,7 @@
 	<script src="/js/jquery.parallax.js"></script>
 	<script src="/js/smoothscroll.js"></script>
 	<script src="/js/nivo-lightbox.min.js"></script>
-	<script src="/s/wow.min.js"></script>
+	<script src="/js/wow.min.js"></script>
 	<script src="/js/custom.js"></script>
 
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
